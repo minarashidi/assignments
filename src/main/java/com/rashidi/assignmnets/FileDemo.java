aff267c69bc3a0ba183902db23be0ddadc52e94b
minarashidi <mina.rashidi.86@gmail.com>
2017-10-15 14:49:00 +0200

    Counting number of lines, words, characters and paragraphs in a text file

---
+++ src/main/java/com/rashidi/assignmnets/FileDemo.java
@@ -0,0 +1,63 @@
+package com.rashidi.assignmnets;
+
+import java.io.*;
+import java.util.Arrays;
+
+/**
+ * Counting number of lines, words, characters and paragraphs in a text file
+ *
+ * @author Mina Rashidi
+ */
+public class FileDemo {
+    public static void main(String[] args) throws IOException {
+
+        File file = new File("test.txt");
+        FileInputStream fileStream = new FileInputStream(file);
+        InputStreamReader input = new InputStreamReader(fileStream);
+        BufferedReader reader = new BufferedReader(input);
+
+        String line;
+
+        // Initializing counters
+        int countWord = 0;
+        int sentenceCount = 0;
+        int characterCount = 0;
+        int paragraphCount = 1;
+        int whitespaceCount = 0;
+
+        // Reading line by line from the
+        // file until a null is returned
+        while((line = reader.readLine()) != null)
+        {
+            if(line.equals(""))
+            {
+                paragraphCount++;
+            }
+            if(!(line.equals("")))
+            {
+
+                characterCount += line.length();
+
+                // \\s+ is the space delimiter in java
+                String[] wordList = line.split("\\s+");
+
+                countWord += wordList.length;
+                whitespaceCount += countWord -1;
+
+                // [!?.:]+ is the sentence delimiter in java
+                String[] sentenceList = line.split("[!?.:]+");
+//                for (String s:sentenceList){
+//                    System.out.println(Arrays.toString(sentenceList));
+//                }
+
+                sentenceCount += sentenceList.length;
+            }
+        }
+
+        System.out.println("Total word count = " + countWord);
+        System.out.println("Total number of sentences = " + sentenceCount);
+        System.out.println("Total number of characters = " + characterCount);
+        System.out.println("Number of paragraphs = " + paragraphCount);
+        System.out.println("Total number of whitespaces = " + whitespaceCount);
+    }
+}
